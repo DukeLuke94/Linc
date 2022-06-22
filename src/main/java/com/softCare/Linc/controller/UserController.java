d14e66d9090d27ebac8aa29d27349735af310d8c
DukeLuke94 <94402446+DukeLuke94@users.noreply.github.com>
2022-06-22 12:09:16 +0200

    added user profile page

--- src/main/java/com/softCare/Linc/controller/UserController.java
+++ src/main/java/com/softCare/Linc/controller/UserController.java
@@ -1,27 +1,28 @@
 package com.softCare.Linc.controller;
 
-import com.softCare.Linc.model.Circle;
 import com.softCare.Linc.model.User;
 import com.softCare.Linc.service.LincUserDetailServiceInterface;
+import org.springframework.security.core.Authentication;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class UserController {
 
-    private final LincUserDetailServiceInterface lincUserDetailServiceInterface;
+    private final LincUserDetailServiceInterface userInterface;
     private final PasswordEncoder passwordEncoder;
 
 
-    public UserController(LincUserDetailServiceInterface lincUserDetailServiceInterface, PasswordEncoder passwordEncoder) {
-        this.lincUserDetailServiceInterface = lincUserDetailServiceInterface;
+    public UserController(LincUserDetailServiceInterface userInterface, PasswordEncoder passwordEncoder) {
+        this.userInterface = userInterface;
         this.passwordEncoder = passwordEncoder;
     }
+
 
     @GetMapping({"/user/new"})
     protected String newUser(Model model) {
@@ -34,9 +35,17 @@ public class UserController {
             return "userForm";
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        lincUserDetailServiceInterface.save(user);
+        userInterface.save(user);
         return "redirect:/home";
     }
+
+
+    @RequestMapping(value = "/user/profile", method = RequestMethod.GET)
+    public String currentUserName(Authentication authentication, Model model) {
+        model.addAttribute("user",userInterface.loadUserByUsername(authentication.getName()));
+        return "userProfile";
+    }
+
 
 
 
