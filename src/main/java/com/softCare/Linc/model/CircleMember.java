d319d96be05652cb37a77c36f83ddc738d095901
DukeLuke94 <94402446+DukeLuke94@users.noreply.github.com>
2022-06-28 15:40:43 +0200

    SQL fix

--- src/main/java/com/softCare/Linc/model/CircleMember.java
+++ src/main/java/com/softCare/Linc/model/CircleMember.java
@@ -2,6 +2,8 @@ package com.softCare.Linc.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -21,6 +23,7 @@ public class CircleMember {
     private User user;
 
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Circle circle;
 
     private boolean isClient;
