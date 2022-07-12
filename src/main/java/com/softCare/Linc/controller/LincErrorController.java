2279265951790e6d5d73ccbcf56e2d0e47466113
GeraldGitHubAccount <gghrtimmermans@gmail.com>
2022-07-12 10:26:59 +0200

    commented out custom error pages to make it easier for development of product

--- src/main/java/com/softCare/Linc/controller/LincErrorController.java
+++ src/main/java/com/softCare/Linc/controller/LincErrorController.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class LincErrorController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error404";
-            }
-        }
-        return "customError";
-    }
-}
+//@Controller
+//public class LincErrorController implements ErrorController {
+//
+//    @RequestMapping("/error")
+//    public String handleError(HttpServletRequest request) {
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//        if (status != null) {
+//            Integer statusCode = Integer.valueOf(status.toString());
+//
+//            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+//                return "error404";
+//            }
+//        }
+//        return "customError";
+//    }
+//}
