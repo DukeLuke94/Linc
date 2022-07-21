e1c5701d6c3d1ca5bce58b94d8c4af54e5c8df0b
DukeLuke94 <94402446+DukeLuke94@users.noreply.github.com>
2022-07-21 15:27:58 +0200

    LN-102 400 error page

--- src/main/java/com/softCare/Linc/controller/LincErrorController.java
+++ src/main/java/com/softCare/Linc/controller/LincErrorController.java
@@ -8,21 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-//@Controller
-//public class LincErrorController implements ErrorController {
-//
-//    @RequestMapping("/error")
-//    public String handleError(HttpServletRequest request) {
-//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//        if (status != null) {
-//            int statusCode = Integer.parseInt(status.toString());
-//
-//            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-//                return "error404";
-//            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-//                return "error500";
-//            }
-//        }
-//        return "customError";
-//    }
-//}
+@Controller
+public class LincErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error500";
+            } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error400";
+            }
+
+        }
+        return "customError";
+    }
+}
