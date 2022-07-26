06c78b21f642cf98f6daa3564c803e5ea79ece1a
Jan Willem van der Wal <jwvdw89@gmail.com>
2022-07-26 11:50:12 +0200

    Changed message when passwords are not equal + this message is only showed when the password is long enough

--- src/main/java/com/softCare/Linc/controller/UserController.java
+++ src/main/java/com/softCare/Linc/controller/UserController.java
@@ -32,7 +32,9 @@ public class UserController {
     public final String EDIT_SUCCESSFUL = "Password edit successful, please login with your new password";
     public final String EMAIL_ALREADY_IN_USE = "This Email-address is already in use";
     public final String CURRENT_PASSWORD_IS_NOT_CORRECT = "The current password is not correct";
-    private final String PASSWORD_REPEAT_NO_MATCH = "The newly entered passwords are not an exact match or aren't given";
+    private final String PASSWORD_REPEAT_NO_MATCH = "The newly entered passwords are not an exact match";
+
+    private final String PASSWORDS_NOT_GIVEN = "The password-fields cannot be empty";
 
     private final LincUserDetailServiceInterface userInterface;
     private final CircleMemberServiceInterface circleMemberServiceInterface;
@@ -260,7 +262,11 @@ public class UserController {
     }
 
     private boolean newPasswordsDoNotMatch(UserVmGeneral userVmGeneral) {
-        return !userVmGeneral.getPassword().equals(userVmGeneral.getPasswordRepeat());
+        return  !userVmGeneral.getPassword().equals(userVmGeneral.getPasswordRepeat()) &&
+                !userVmGeneral.getPassword().isBlank() &&
+                !userVmGeneral.getPasswordRepeat().isBlank() &&
+                userVmGeneral.getPassword().length() >= 6 &&
+                userVmGeneral.getPasswordRepeat().length() >= 6;
     }
 
     @GetMapping({"/sysAdmin/users"})
